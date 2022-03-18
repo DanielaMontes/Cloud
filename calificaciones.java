923f06296a74432aab475314035b17c8d2924875
DanielaMontes <66233799+DanielaMontes@users.noreply.github.com>
2022-03-18 00:23:56 -0600

    Update calificaciones.java

--- calificaciones.java
+++ calificaciones.java
@@ -3,31 +3,27 @@ import java.util.Scanner ;
 
 public class promedio {
 public static void main (String[]args) {
-int cal1;
-int cal2; 
-int cal3;
-int suma;
-int promedio ;
+int cal1, cal2, cal3,suma,promedio ;
 
 Scanner entrada = new Scanner (System.in) ;
 
 System.out.println ("Ingrese calificacion 1" ;
 cal1 = entrada.nextInt() ;
 
-System.out.println ("Ingrese nota 2" ;
+System.out.println ("Ingrese calificacion 2" ;
 cal2 = entrada.nextInt() ;
 
-System.out.println ("Ingrese nota 3" ;
+System.out.println ("Ingrese calificacion 3" ;
 cal3 = entrada.nextInt() ;
 
 suma = (cal1 + cal2 + cal3) ;
 promedio = (suma/3) ;
 
 if (promedio >= 7) {
-System.out.println ("El estudiante aprovo con un promedio de: " +promedio ) ;
+System.out.println ("El estudiante aprobó con un promedio de: " +promedio ) ;
 }
 else {
-System.out.println ("El estudiante no aprovo, el promedio es de: " +promedio ) ;
+System.out.println ("El estudiante no aprobó, el promedio es de: " +promedio ) ;
 }
 }
 }
